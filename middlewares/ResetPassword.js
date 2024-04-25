8c958eb9b80b40c8c18d92f86553529c9712f6ea
Shubham <shubhambhardwaj112277@gmail.com>
2024-04-25 20:52:15 +0530

    Implement password reset functionality including sending reset password links via email and handling password reset endpoint in the backend.

--- middlewares/ResetPassword.js
+++ middlewares/ResetPassword.js
@@ -1,7 +1,89 @@
-
-
+const User = require('../models/User')
+const mailSender = require('../utils/mailSender')
+const bcrypt = require('bcrypt')
 
 //resetPasswordToken
+exports.resetPasswordToken = async (req,res) =>{
+    try {
+           //get email from req body
+    const email = req.body.email;
+    //check user for this email, email validation
+    const user = await User.findOne({email})
 
+    if(!user){
+        return res.json({
+            success:false,
+            message:'Your Email is not registered with us'
+        })
+    }
+    //generate token
+    const token = crypto.randomUUID()
+    //update user by adding token and expirating time
+    const updatedDetails = await User.findOneAndUpdate({email},{token,resetPasswordToken:Date.now() + 5*60*1000},{new:true})
+    //create url
+    const url = `http://localhost:3000/update-password/${token}`
+    //send mail containing the url
+    await mailSender(email,"Password Reset Link",`Password Reset Link: ${url}`)
+    // return response 
+    return res.json({
+        success:true,
+        message:"Email sent successfully, please check email and password"
+    })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message:'Something went wrong while sending reset password mail'
+        })
+    }
+
+}
 
 //resetPassword
+
+exports.resetPassword = async (req,res)=>{
+    try {
+          //data fetch
+    const {password,confirmPassword,token} = req.body;
+    //validation
+    if(password !== confirmPassword){
+        return res.json({
+            success:false,
+            message:'Password not matching'
+        })
+    }
+    //get userdetails from db using token
+    const userDetails = await User.findOne({token});
+    //if no entry - invalid token
+    if(!userDetails){
+        return res.json({
+            success:false,
+            message:'Token is invalid',
+        })
+    }
+    //token time check
+    if(userDetails.resetPasswordExpires < Date.now()){
+        return res.json({
+            success:false,
+            message:'Token is expired, please regenerate your token'
+        })
+    }
+    //hash password
+    const hashedPassword = await bcrypt.hash(password,10);
+    //update password
+    await User.findOneAndUpdate({token,password:hashedPassword},{new:true})
+    //return response
+    return res.status(200).json({
+        success:true,
+        message:'Password reset successful',
+    })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message:'Something went wrong while  reset password '
+        })
+    }
+  
+}
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -18,9 +18,9 @@ exports.auht = async (req,res,next) =>{
 
         //verify the token
         try {
-            const decode = await jwt.verify(token,process.env.JWT_SECRET);
+            const decode =  jwt.verify(token,process.env.JWT_SECRET);
             console.log(decode);
-            req.body.user = decode
+            req.user = decode
         } catch (error) {
             //verification - issue
             return res.status(401).json({
@@ -29,14 +29,69 @@ exports.auht = async (req,res,next) =>{
             })
         }
 
+        next()
+
     } catch (error) {
-        
+        //verification - issue
+        return res.status(401).json({
+            success:false,
+            message:'Something went wrong while validating the token'
+        })
     }
 }
 
 //isStudent
+exports.isStudent = async (req,res,next) =>{
+    try {
+        if(req.user.accountType !== "Student"){
+            return res.status(401).json({
+                success:false,
+                message:'This is a protected route for Students only',
+            })
+        }
+        
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:'User role cannot be verified,please try again',
+        })
+    }
+}
 
 //isInstructor
+exports.isInstructor = async (req,res,next) =>{
+    try {
+        if(req.user.accountType !== "Instructor"){
+            return res.status(401).json({
+                success:false,
+                message:'This is a protected route for Instructors only',
+            })
+        }
+        
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:'User role cannot be verified,please try again',
+        })
+    }
+}
+
 
 
 //isAdmin
+exports.isAdmin = async (req,res,next) =>{
+    try {
+        if(req.user.accountType !== "Admin"){
+            return res.status(401).json({
+                success:false,
+                message:'This is a protected route for Admin only',
+            })
+        }
+        
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:'User role cannot be verified,please try again',
+        })
+    }
+}
