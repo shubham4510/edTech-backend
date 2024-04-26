d8a1abdcd4659efc68a31b285e9ff54d82161ce6
Shubham <shubhambhardwaj112277@gmail.com>
2024-04-26 14:24:49 +0530

    Added createCourse and showAllCourses handlers in backend project

--- models/Tags.js
+++ models/Tags.js
@@ -10,10 +10,12 @@ const tagsSchema = new mongoose.Schema({
     description:{
      type:String,
     },
-    course:{
+    course:[
+        {
      type:mongoose.Schema.Types.ObjectId,
      ref:"Course",
     }
+]
  })
  
  module.exports = mongoose.model("Tag",tagsSchema)
