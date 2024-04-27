974859da9066d3313e48d3acdc59835a72f1abd8
Shubham <shubhambhardwaj112277@gmail.com>
2024-04-27 13:42:11 +0530

    Implemented categoryPageDetails handler in Category

--- controllers/Category.js
+++ controllers/Category.js
@@ -53,3 +53,44 @@ exports.showAllCategory = async (req,res) =>{
         })
     }
 }
+
+
+//categoryPageDetails
+exports.categoryPageDetails = async (req,res) => {
+    try {
+        //get categoryId
+        const {categoryId} = req.body;
+        // get courses for specified categoryId
+        const selectedCategory = await Category.findById(categoryId).populate("courses")
+                                                                    .exec();
+        //validation
+        if(!selectedCategory){
+            return res.status(404).json({
+                success:false,
+                message:'Data Not Found',
+            })
+        }
+
+        //get courses for different categories
+        const differentCategories = await Category.find({
+            _id: {$ne: categoryId}
+        }).populate("courses").exec();
+
+        //get top 10 selling courses
+        //HW: write it on your own
+        //return response
+        return res.status(200).json({
+            success:true,
+            data:{
+                selectedCategory,
+                differentCategories,
+            },
+        })
+        
+    } catch (error) {
+        return res.status(500).json({
+            status:false,
+            message:error.message,
+        })
+    }
+}
---
+++ controllers/Contact.js
@@ -0,0 +1,1 @@
+//HW: send a mail to yourself and to children that your data is received
